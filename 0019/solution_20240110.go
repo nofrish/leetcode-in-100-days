0020673b360fb1c975fe74a749c34fae2f51723e
nofrish <nofrish@gmail.com>
2024-01-10 18:17:08 +0800

    finished 0019.

---
+++ 0019/solution_20240110.go
@@ -0,0 +1,33 @@
+package _019
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+
+	dummy := &ListNode{0, head}
+
+	slow, fast := dummy, dummy
+	for i := 0; i < n; i++ {
+		fast = fast.Next
+	}
+
+	for fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next
+	}
+
+	slow.Next = slow.Next.Next
+
+	return dummy.Next
+}
