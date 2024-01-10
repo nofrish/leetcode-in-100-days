c4fcb2f8920587f8636b0ce8a82336ad7099201f
nofrish <nofrish@gmail.com>
2024-01-10 18:00:03 +0800

    finished 0203. recursively.

--- 0203/solution_20240110.go
+++ 0203/solution_20240110.go
@@ -13,6 +13,7 @@ type ListNode struct {
 	Next *ListNode
 }
 
+// Solution 2. Iteratively
 func removeElements(head *ListNode, val int) *ListNode {
 
 	dummy := &ListNode{0, head}
@@ -24,4 +25,22 @@ func removeElements(head *ListNode, val int) *ListNode {
 		}
 	}
 	return dummy.Next
+}
+
+// Solution 2. Recursively
+func removeElementsRecursively(head *ListNode, val int) *ListNode {
+	dummy := &ListNode{0, head}
+	return recursivelyRemove(dummy, dummy.Next, val)
+}
+
+func recursivelyRemove(prev, cur *ListNode, val int) *ListNode {
+	if cur == nil {
+		return nil
+	}
+	if cur.Val != val {
+		cur.Next = recursivelyRemove(cur, cur.Next, val)
+		return cur
+	} else {
+		return recursivelyRemove(prev, cur.Next, val)
+	}
 }
