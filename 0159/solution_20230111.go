1550e6c7147ffd1146f035ab77df274012282d09
nofrish <nofrish@gmail.com>
2024-01-11 11:07:26 +0800

    updated 0159

--- 0159/solution_20230111.go
+++ 0159/solution_20230111.go
@@ -6,15 +6,12 @@ func lengthOfLongestSubstringTwoDistinct(s string) int {
 
 	slow, fast, exists, max := 0, -1, make(map[byte]int), 0
 	for fast++; fast < len(s); fast++ {
-		c := s[fast]
-		for ; exists[c] == 0 && len(exists) == 2; slow++ {
+		for exists[s[fast]] += 1; len(exists) > 2; slow++ {
 			exists[s[slow]] -= 1
 			if exists[s[slow]] == 0 {
 				delete(exists, s[slow])
 			}
 		}
-		exists[c] += 1
-
 		max = int(math.Max(float64(max), float64(fast-slow+1)))
 	}
 	return max
