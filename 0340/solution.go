dfce9d095805db87daaa808b9a45a4b0b65cd270
nofrish <nofrish@gmail.com>
2024-01-11 15:20:58 +0800

    updated 0340

--- 0340/solution.go
+++ 0340/solution.go
@@ -5,6 +5,7 @@ import "math"
 func _lengthOfLongestSubstringKDistinct(s string, k int) int {
 	slow, fast, exists, max := 0, -1, make(map[byte]int), 0
 	for fast++; fast < len(s); fast++ {
+		// 先将新的值加入进来，再判断窗口的状态是否符合要求，如果不符合要求，则移动slow，直至符合要求
 		for exists[s[fast]] += 1; len(exists) > k; slow++ {
 			exists[s[slow]] -= 1
 			if exists[s[slow]] == 0 {
