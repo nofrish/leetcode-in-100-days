11d79e88a1ea8b425af8f7279f209dbfa3646687
nofrish <nofrish@gmail.com>
2024-01-15 10:45:18 +0800

    finished 0144 again. recursively.

---
+++ 0144/solution_20240115.go
@@ -0,0 +1,17 @@
+package _144
+
+var result = make([]int, 0)
+
+func preorderTraversal_(root *TreeNode) []int {
+	result = result[0:0:0]
+	dfs(root)
+	return result
+}
+
+func dfs(node *TreeNode) {
+	if node != nil {
+		result = append(result, node.Val)
+		dfs(node.Left)
+		dfs(node.Right)
+	}
+}
