2ad643445a8bc884b52ed35e41b3f0b95d1110f2
nofrish <nofrish@gmail.com>
2024-01-16 10:27:37 +0800

    finished 0270

---
+++ 0270/solution_20240116.go
@@ -0,0 +1,37 @@
+package _270
+
+import "math"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func closestValue(root *TreeNode, target float64) (result int) {
+
+	min := math.MaxFloat64
+	for root != nil {
+		abs := math.Abs(float64(root.Val) - target)
+		if (min > abs) || (min == abs && root.Val < result) {
+			min = abs
+			result = root.Val
+		}
+		if float64(root.Val) > target {
+			root = root.Left
+		} else {
+			root = root.Right
+		}
+	}
+
+	return
+}
