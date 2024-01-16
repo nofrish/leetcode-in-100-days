d0eb249883d89e251268dc5353dcad9aa2b42347
nofrish <nofrish@gmail.com>
2024-01-16 18:34:46 +0800

    updated 0098. add another template.

--- 0098/solution.go
+++ 0098/solution.go
@@ -25,3 +25,23 @@ func _isValidBST(root *TreeNode) bool {
 
 	return dfs(root)
 }
+
+// Solution Two.
+func __isValidBST(root *TreeNode) bool {
+
+	var dfs func(node *TreeNode, lowerBound, upperBound int) bool
+	dfs = func(node *TreeNode, lowerBound, upperBound int) bool {
+		if node == nil {
+			return true
+		}
+		if node.Val <= lowerBound {
+			return false
+		}
+		if node.Val >= upperBound {
+			return false
+		}
+		return dfs(node.Left, lowerBound, node.Val) && dfs(node.Right, node.Val, upperBound)
+	}
+
+	return dfs(root, math.MinInt, math.MaxInt)
+}
