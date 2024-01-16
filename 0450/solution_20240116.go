2600faadfc009c8ee1a1aa9ee945ea4b32c0c70e
nofrish <nofrish@gmail.com>
2024-01-16 11:20:27 +0800

    finished 0450

---
+++ 0450/solution_20240116.go
@@ -0,0 +1,48 @@
+package _450
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func deleteNode(root *TreeNode, key int) *TreeNode {
+
+	if root == nil {
+		return nil
+	}
+	if root.Val == key {
+		if root.Left == nil {
+			return root.Right
+		}
+		if root.Right == nil {
+			return root.Left
+		}
+		root.Val = findMinVal(root.Right)
+		root.Right = deleteNode(root.Right, root.Val)
+	}
+
+	if key < root.Val {
+		root.Left = deleteNode(root.Left, key)
+	} else {
+		root.Right = deleteNode(root.Right, key)
+	}
+
+	return root
+}
+
+func findMinVal(root *TreeNode) int {
+	for root.Left != nil {
+		root = root.Left
+	}
+	return root.Val
+}
