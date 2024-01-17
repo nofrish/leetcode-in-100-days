bf7a150b6107490200da65bb4caad7aa665751fc
nofrish <nofrish@gmail.com>
2024-01-17 10:44:54 +0800

    0056

---
+++ 0056/solution_20240117.go
@@ -0,0 +1,27 @@
+package _056
+
+import (
+	"math"
+	"sort"
+)
+
+func merge_(intervals [][]int) [][]int {
+
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	result := make([][]int, 0)
+	result = append(result, intervals[0])
+
+	for _, interval := range intervals[1:] {
+		last := result[len(result)-1]
+		if last[1] >= interval[0] {
+			last[1] = int(math.Max(float64(last[1]), float64(interval[1])))
+		} else {
+			result = append(result, interval)
+		}
+	}
+
+	return result
+}
