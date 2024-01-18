7bcd8741aea937cd172a1d56bae4244b40f9ab4b
nofrish <nofrish@gmail.com>
2024-01-18 22:58:11 +0800

    finished 0046

---
+++ 0046/solution_20240118.go
@@ -0,0 +1,18 @@
+package _046
+
+func permute(nums []int) (result [][]int) {
+
+	var dfs func(prefix []int, remains []int)
+	dfs = func(prefix []int, remains []int) {
+		if len(remains) == 0 {
+			result = append(result, prefix)
+			return
+		}
+		for i, v := range remains {
+			dfs(append(prefix, v), append(append([]int{}, remains[0:i]...), remains[i+1:]...))
+		}
+	}
+
+	dfs([]int{}, nums)
+	return
+}
