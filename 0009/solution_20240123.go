b1ce3b5cf1d8a5daace0055d04e6260b38ad3278
nofrish <nofrish@gmail.com>
2024-01-23 17:23:32 +0800

    finished 0009 again

--- 0009/solution_20240123.go
+++ 0009/solution_20240123.go
@@ -10,10 +10,23 @@ func isPalindrome(x int) bool {
 
 	var y int64
 	for x > 0 {
-		r := x % 10
-		y = y*10 + int64(r)
+		y = y*10 + int64(x%10)
 		x = x / 10
 	}
 
 	return y == t
+}
+
+func isPalindrome_(x int) bool {
+
+	if x < 0 || (x != 0 && x%10 == 0) {
+		return false
+	}
+
+	t := 0
+	for x > t {
+		t = t*10 + x%10
+		x = x / 10
+	}
+	return x == t || x == t/10
 }
