773839129510cedb05dad447f8408bf6bb0d3c06
nofrish <nofrish@gmail.com>
2024-01-29 10:32:54 +0800

    finished 0395, the divider-and-conquer way.

--- 0395/solution_20240116.go
+++ 0395/solution_20240116.go
@@ -1,6 +1,42 @@
 package _395
 
+import (
+	"strings"
+)
+
 func longestSubstring(s string, k int) int {
+
+	counts := make(map[byte]int, 0)
+	for i := 0; i < len(s); i++ {
+		counts[s[i]] += 1
+	}
+
+	var split byte
+	for c, v := range counts {
+		if v < k {
+			split = c
+			break
+		}
+	}
+	if split == 0 {
+		return len(s)
+	}
+
+	result := 0
+	for _, subStr := range strings.Split(s, string(split)) {
+		result = max(result, longestSubstring(subStr, k))
+	}
+	return result
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func longestSubstring_(s string, k int) int {
 
 	max := 0
 	for i := 1; i <= 26; i++ {
