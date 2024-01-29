83d3a5a3658bb145e749b30719dbc6be3bc08a9b
nofrish <nofrish@gmail.com>
2024-01-29 19:06:56 +0800

    finished 0076 again

---
+++ 0076/solution_20240129.go
@@ -0,0 +1,30 @@
+package _076
+
+func minWindow_(s string, t string) string {
+
+	satisfied := func(src, tgt map[byte]int) bool {
+		for k, v := range tgt {
+			if src[k] < v {
+				return false
+			}
+		}
+		return true
+	}
+
+	tgt := make(map[byte]int, 0)
+	for i := 0; i < len(t); i++ {
+		tgt[t[i]] += 1
+	}
+
+	slow, fast, result, exists := 0, -1, "", make(map[byte]int, 0)
+	for fast++; fast < len(s); fast++ {
+		exists[s[fast]] += 1
+		for ; satisfied(exists, tgt); slow++ {
+			if result == "" || len(result) > fast-slow+1 {
+				result = s[slow : fast+1]
+			}
+			exists[s[slow]] -= 1
+		}
+	}
+	return result
+}
