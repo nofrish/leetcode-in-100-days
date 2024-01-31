d3ba218e810fa191abc6c1b5273247b290a5f829
nofrish <nofrish@gmail.com>
2024-01-31 17:06:12 +0800

    finished 0042

---
+++ 0042/solution_20240131.go
@@ -0,0 +1,29 @@
+package _042
+
+func trap(height []int) (result int) {
+
+	stack := make([]int, 0)
+	for r, v := range height {
+		for len(stack) > 0 && v > height[stack[len(stack)-1]] {
+			mid := stack[len(stack)-1]
+
+			stack = stack[:len(stack)-1]
+			if len(stack) == 0 {
+				break
+			}
+			l := stack[len(stack)-1]
+
+			area := (r - l - 1) * (min(height[l], height[r]) - height[mid])
+			result += area
+		}
+		stack = append(stack, r)
+	}
+	return
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
