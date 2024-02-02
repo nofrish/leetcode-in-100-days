d0cb95bd6e58f9daeeffcf440ad4e7b536c54c62
nofrish <nofrish@gmail.com>
2024-02-02 14:26:39 +0800

    finished 0746

---
+++ 0746/solution_20240202.go
@@ -0,0 +1,21 @@
+package _746
+
+func minCostClimbingStairs(cost []int) int {
+
+	dp := make([]int, len(cost))
+	dp[0] = cost[0]
+	dp[1] = cost[1]
+
+	for i := 2; i < len(cost); i++ {
+		dp[i] = cost[i] + min(dp[i-1], dp[i-2])
+	}
+
+	return min(dp[len(cost)-1], dp[len(cost)-2])
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
