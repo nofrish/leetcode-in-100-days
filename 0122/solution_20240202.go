cffde60641ad66384c4f0690704a0827578bc563
nofrish <nofrish@gmail.com>
2024-02-02 20:57:54 +0800

    finished 0122

---
+++ 0122/solution_20240202.go
@@ -0,0 +1,22 @@
+package _122
+
+func maxProfit(prices []int) int {
+
+	dp := make([][2]int, len(prices))
+	dp[0][0] = -prices[0]
+	dp[0][1] = 0
+
+	for i := 1; i < len(prices); i++ {
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1]-prices[i])
+		dp[i][1] = max(prices[i]+dp[i-1][0], dp[i-1][1])
+	}
+
+	return max(dp[len(prices)-1][0], dp[len(prices)-1][1])
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
