1dbfb7e2a429db095804aea9c6032968a55fc4ed
nofrish <nofrish@gmail.com>
2024-02-04 11:33:54 +0800

    finished 0014

---
+++ 0014/solution_20240204.go
@@ -0,0 +1,14 @@
+package _014
+
+func longestCommonPrefix(strs []string) string {
+
+	for i := 0; true; i++ {
+		for j := 0; j < len(strs); j++ {
+			if i == len(strs[j]) || strs[j][i] != strs[0][i] {
+				return strs[0][:i]
+			}
+		}
+	}
+
+	return strs[0]
+}
