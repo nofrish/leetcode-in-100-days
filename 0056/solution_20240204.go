a15f6f27693ad2a6e50a10fc930e8dbb6ae16237
nofrish <nofrish@gmail.com>
2024-02-04 23:54:51 +0800

    finished 0056 again.

--- 0056/solution_20240204.go
+++ 0056/solution_20240204.go
@@ -19,3 +19,20 @@ func merge___(intervals [][]int) [][]int {
 	}
 	return result
 }
+
+func merge____(intervals [][]int) [][]int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	slow, fast := 0, 0
+	for fast++; fast < len(intervals); fast++ {
+		if intervals[slow][1] >= intervals[fast][0] {
+			intervals[slow][1] = max(intervals[slow][1], intervals[fast][1])
+		} else {
+			slow++
+			intervals[slow] = intervals[fast]
+		}
+	}
+	return intervals[:slow+1]
+}
