471558dae4816fc28b4f99ff6abc1368586c1565
nofrish <nofrish@gmail.com>
2024-02-07 00:23:06 +0800

    finished 0049

---
+++ 0049/solution_20240207.go
@@ -0,0 +1,33 @@
+package _049
+
+import "strings"
+
+func groupAnagrams(strs []string) [][]string {
+
+	m := make(map[string][]string)
+	for _, str := range strs {
+		chars := [26]int{}
+		for i := 0; i < len(str); i++ {
+			chars[int(str[i]-'a')] += 1
+		}
+		sb := strings.Builder{}
+		for i := 0; i < 26; i++ {
+			for j := 0; j < chars[i]; j++ {
+				sb.WriteByte('a' + byte(i))
+			}
+		}
+		key := sb.String()
+
+		if v, ok := m[key]; ok {
+			m[key] = append(v, str)
+		} else {
+			m[key] = []string{str}
+		}
+	}
+
+	result := make([][]string, 0)
+	for _, v := range m {
+		result = append(result, v)
+	}
+	return result
+}
