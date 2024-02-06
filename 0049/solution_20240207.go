41fb83cfcb8d402cdcd17ad10c47a6502656ac42
nofrish <nofrish@gmail.com>
2024-02-07 00:30:06 +0800

    finished 0049

--- 0049/solution_20240207.go
+++ 0049/solution_20240207.go
@@ -3,6 +3,23 @@ package _049
 import "strings"
 
 func groupAnagrams(strs []string) [][]string {
+	m := make(map[[26]int][]string)
+	for _, str := range strs {
+		chars := [26]int{}
+		for i := 0; i < len(str); i++ {
+			chars[int(str[i]-'a')] += 1
+		}
+		m[chars] = append(m[chars], str)
+	}
+
+	result := make([][]string, 0, len(m))
+	for _, v := range m {
+		result = append(result, v)
+	}
+	return result
+}
+
+func groupAnagrams_(strs []string) [][]string {
 
 	m := make(map[string][]string)
 	for _, str := range strs {
