27a0028042760ce7d051c3bc2f30e588c9d4cb33
nofrish <nofrish@gmail.com>
2024-02-19 14:32:10 +0800

    finished 0015 again.

---
+++ 0015/solution_20240219.go
@@ -0,0 +1,42 @@
+package _015
+
+import "sort"
+
+func threeSum__(nums []int) (result [][]int) {
+
+	sort.Ints(nums)
+
+	for i := range nums {
+		if nums[i] > 0 {
+			break
+		}
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+
+		j, k := i+1, len(nums)-1
+		for j < k {
+			if j > i+1 && nums[j] == nums[j-1] {
+				j++
+				continue
+			}
+			if k < len(nums)-1 && nums[k] == nums[k+1] {
+				k--
+				continue
+			}
+
+			sum := nums[i] + nums[j] + nums[k]
+			if sum > 0 {
+				k--
+			} else if sum < 0 {
+				j++
+			} else {
+				result = append(result, []int{nums[i], nums[j], nums[k]})
+				j++
+				k--
+			}
+		}
+	}
+
+	return
+}
