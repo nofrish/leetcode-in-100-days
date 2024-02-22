afd6380b4f11c9de5bb73d3bc107520f38e19394
nofrish <nofrish@gmail.com>
2024-02-22 10:13:13 +0800

    one take: finished 0144 again.

--- 0144/solution_20240222.go
+++ 0144/solution_20240222.go
@@ -26,3 +26,18 @@ func preorderTraversal___(root *TreeNode) []int {
 	dfs(root)
 	return result
 }
+
+func preorderTraversal____(root *TreeNode) (result []int) {
+
+	nexts := []*TreeNode{root}
+	for len(nexts) > 0 {
+		node := nexts[len(nexts)-1]
+		nexts = nexts[:len(nexts)-1]
+		if node != nil {
+			result = append(result, node.Val)
+			nexts = append(nexts, node.Right)
+			nexts = append(nexts, node.Left)
+		}
+	}
+	return
+}
