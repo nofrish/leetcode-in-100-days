3e27d15474956d2ff45d8c47cb5fed0f7464609a
nofrish <nofrish@gmail.com>
2024-02-22 11:08:30 +0800

    finished 0094 again. learned something new.

--- 0094/solution_20240222.go
+++ 0094/solution_20240222.go
@@ -23,3 +23,20 @@ func inorderTraversal_(root *TreeNode) (result []int) {
 	dfs(root)
 	return
 }
+
+func inorderTraversal__(root *TreeNode) (result []int) {
+
+	nexts := make([]*TreeNode, 0)
+	for root != nil || len(nexts) != 0 {
+		if root != nil {
+			nexts = append(nexts, root)
+			root = root.Left
+		} else {
+			node := nexts[len(nexts)-1]
+			nexts = nexts[:len(nexts)-1]
+			result = append(result, node.Val)
+			root = node.Right
+		}
+	}
+	return
+}
