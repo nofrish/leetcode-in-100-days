4e0a5efeb4dc0e578edeee8fcbdd5e1bd40fcde7
nofrish <nofrish@gmail.com>
2024-02-22 20:13:02 +0800

    one take: finished 0226 again.

--- 0226/solution_20240222.go
+++ 0226/solution_20240222.go
@@ -30,3 +30,17 @@ func invertTree(root *TreeNode) *TreeNode {
 	}
 	return root
 }
+
+func invertTree_(root *TreeNode) *TreeNode {
+	var dfs func(node *TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		node.Left, node.Right = node.Right, node.Left
+		dfs(node.Left)
+		dfs(node.Right)
+	}
+	dfs(root)
+	return root
+}
