b78bcad75c4118bab579cf1d2dc6d1b1e80143b6
nofrish <nofrish@gmail.com>
2024-02-24 11:18:12 +0800

    one take: finished 0700 again. but not in the right way.

---
+++ 0700/solution_20240224.go
@@ -0,0 +1,32 @@
+package _700
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func searchBST_(root *TreeNode, val int) (result *TreeNode) {
+
+	var dfs func(node *TreeNode) bool
+	dfs = func(node *TreeNode) bool {
+		if node == nil {
+			return false
+		}
+		if dfs(node.Left) || dfs(node.Right) {
+			return true
+		}
+		if node.Val == val {
+			result = node
+			return true
+		}
+		return false
+	}
+	if !dfs(root) {
+		result = nil
+	}
+	return
+}
