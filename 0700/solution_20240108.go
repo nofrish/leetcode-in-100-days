5fb775436f5067775510805323b8c26b4332259e
nofrish <nofrish@gmail.com>
2024-02-26 14:42:41 +0800

    updated 0700

--- 0700/solution_20240108.go
+++ 0700/solution_20240108.go
@@ -17,13 +17,9 @@ type TreeNode struct {
 
 func searchBST(root *TreeNode, val int) *TreeNode {
 
-	if root == nil {
-		return nil
-	}
-	if root.Val == val {
+	if root == nil || root.Val == val {
 		return root
 	}
-
 	if root.Val > val {
 		return searchBST(root.Left, val)
 	} else {
