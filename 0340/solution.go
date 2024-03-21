a7bb1fe95c7151f937a1b58a8cd87114b1baa1d0
nofrish <nofrish@gmail.com>
2024-03-21 21:43:55 +0800

    0340: updated solution template.

--- 0340/solution.go
+++ 0340/solution.go
@@ -1,18 +1,15 @@
 package _340
 
-import "math"
-
 func _lengthOfLongestSubstringKDistinct(s string, k int) int {
-	slow, fast, exists, max := 0, -1, make(map[byte]int), 0
+	slow, fast, exists, maxLen := 0, -1, make(map[byte]int), 0
 	for fast++; fast < len(s); fast++ {
-		// 先将新的值加入进来，再判断窗口的状态是否符合要求，如果不符合要求，则移动slow，直至符合要求
 		for exists[s[fast]] += 1; len(exists) > k; slow++ {
 			exists[s[slow]] -= 1
 			if exists[s[slow]] == 0 {
 				delete(exists, s[slow])
 			}
 		}
-		max = int(math.Max(float64(max), float64(fast-slow+1)))
+		maxLen = max(maxLen, fast-slow+1)
 	}
-	return max
+	return maxLen
 }
--- 0340/solution_20240321.go
+++ 0340/solution_20240321.go
@@ -3,8 +3,7 @@ package _340
 func lengthOfLongestSubstringKDistinct___(s string, k int) int {
 	slow, fast, exists, maxLen := 0, -1, make(map[byte]int), 0
 	for fast++; fast < len(s); fast++ {
-		exists[s[fast]] += 1
-		for ; len(exists) > k; slow++ {
+		for exists[s[fast]] += 1; len(exists) > k; slow++ {
 			exists[s[slow]] -= 1
 			if exists[s[slow]] == 0 {
 				delete(exists, s[slow])
