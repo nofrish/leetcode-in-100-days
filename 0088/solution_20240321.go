69575747dd151f516fc81d8c55528189fe3abd65
nofrish <nofrish@gmail.com>
2024-03-21 20:26:56 +0800

    one take: finished 0088 again

---
+++ 0088/solution_20240321.go
@@ -0,0 +1,18 @@
+package _088
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+
+	i, j, cur := m-1, n-1, m+n-1
+	for ; i >= 0 && j >= 0; cur-- {
+		if nums1[i] >= nums2[j] {
+			nums1[cur] = nums1[i]
+			i--
+		} else {
+			nums1[cur] = nums2[j]
+			j--
+		}
+	}
+	for ; j >= 0; j, cur = j-1, cur-1 {
+		nums1[cur] = nums2[j]
+	}
+}
