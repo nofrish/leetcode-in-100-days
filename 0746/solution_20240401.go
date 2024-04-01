68d14d43a16119fb1f15f0fb0c3413e59ca17775
nofrish <nofrish@gmail.com>
2024-04-01 16:24:56 +0800

    finished 0746 again.

--- 0746/solution_20240401.go
+++ 0746/solution_20240401.go
@@ -7,3 +7,11 @@ func minCostClimbingStairs_(cost []int) int {
 	}
 	return min(dp[len(dp)-1], dp[len(dp)-2])
 }
+
+func minCostClimbingStairs__(cost []int) int {
+	pre, cur := cost[0], cost[1]
+	for i := 2; i < len(cost); i++ {
+		pre, cur = cur, cost[i]+min(pre, cur)
+	}
+	return min(pre, cur)
+}
