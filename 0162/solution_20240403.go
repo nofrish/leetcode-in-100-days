7c515053818a155b4393a871533cad05d4899a85
nofrish <nofrish@gmail.com>
2024-04-03 20:15:27 +0800

    updated 0162 comment

--- 0162/solution_20240403.go
+++ 0162/solution_20240403.go
@@ -2,9 +2,10 @@ package _162
 
 func findPeakElement(nums []int) int {
 
+	// 注意：在 [low, high] 中一定有一个峰值，这也是下面low和high迭代的逻辑
 	low, high := 0, len(nums)-1
 	for low < high {
-		// 如果循环条件是 low <= high ，那下面应该有一个退出条件是：
+		// 如果循环条件是 low <= high ，那循环里应该有一个退出条件是：
 		// if low == high { return low }
 		// 如果没有这个退出条件，循环会卡在 low == high == mid 上
 		mid := low + (high-low)/2
