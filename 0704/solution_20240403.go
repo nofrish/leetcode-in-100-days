c592cecb4b78a09ab4a7b0db57bb6a2ea27e615a
nofrish <nofrish@gmail.com>
2024-04-03 08:17:42 +0800

    one take: finished 0704 again

---
+++ 0704/solution_20240403.go
@@ -0,0 +1,16 @@
+package _704
+
+func search_(nums []int, target int) int {
+	low, high := 0, len(nums)-1
+	for low <= high {
+		mid := low + (high-low)/2
+		if nums[mid] < target {
+			low = mid + 1
+		} else if nums[mid] > target {
+			high = mid - 1
+		} else {
+			return mid
+		}
+	}
+	return -1
+}
