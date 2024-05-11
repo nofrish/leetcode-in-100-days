13365840760dac42314a25eb3eea82a4f0a1a061
nofrish <nofrish@gmail.com>
2024-05-11 12:13:12 +0800

    almost one take: finished 0102 again.

---
+++ 0102/solution_20240511.go
@@ -0,0 +1,31 @@
+package _102
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func levelOrder(root *TreeNode) (result [][]int) {
+
+	nexts := []*TreeNode{root}
+	for len(nexts) != 0 {
+		nodes := nexts
+		nexts = make([]*TreeNode, 0)
+		values := make([]int, 0)
+		for _, node := range nodes {
+			if node == nil {
+				continue
+			}
+			values = append(values, node.Val)
+			nexts = append(nexts, node.Left)
+			nexts = append(nexts, node.Right)
+		}
+		if len(values) != 0 {
+			result = append(result, values)
+		}
+	}
+	return
+}
